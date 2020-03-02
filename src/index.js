b230f29904c1e1c2548c58220b9c6127b6aef321
Krukovich <Shaht3r@gmail.com>
2020-03-02 22:32:52 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,61 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const charArray = expr.split("");
+    const countRepeat = expr.length / 10;
+    const morzeArray = [];
+    let finishString = ""
+    const prepareArray = splitArrayTenChar(charArray, countRepeat, newArray = []);
+
+    prepareArray.forEach(miniArray => {
+        const count = miniArray.length / 2;
+        morzeArray.push(prepareArrayToMorze(miniArray, count, result = []));
+    });
+
+    const morzeStrings = translateToMorzeString(morzeArray);
+
+    morzeStrings.forEach(item => {
+        for (const key in MORSE_TABLE) {
+            if (item === "") {
+                finishString += " ";
+                continue;
+            }
+            if (key === item) {
+                finishString += MORSE_TABLE[key];
+            }
+        }
+    }); 
+
+    function translateToMorzeString(morzeArray) {
+        const result = [];
+        morzeArray.forEach(item => {
+            const tempArray = item.map(code => {
+                    if (code === "10") {
+                        return ".";
+                    } else if (code === "11") {
+                        return "-";
+                    } else {
+                        return "";
+                    }
+                });
+                result.push(tempArray.join(""));
+            });
+        return result;
+    }
+
+    function prepareArrayToMorze(array, count, result) {
+        if (count === 0) return result;
+        result.push(array.splice(0, 2).join(""));
+        return prepareArrayToMorze(array, count - 1, result);
+    }
+
+    function splitArrayTenChar(charArray, countRepeat, newArray = []) {
+        if (countRepeat === 0 ) return newArray
+        newArray.push(charArray.splice(0, 10));
+        return splitArrayTenChar(charArray, countRepeat - 1, newArray);
+    }
+
+    return finishString.replace(/\s+/g, " ");
 }
 
 module.exports = {
